22eaa9cf4d40f7f588a6afee1fd7f3b798b518e8
Kyle Allan <kyle@stitchdata.com>
2020-08-06 19:44:32 +0000

    wip sync code for accounts

--- tap_quickbooks/streams.py
+++ tap_quickbooks/streams.py
@@ -4,6 +4,17 @@ class Stream:
         self.config = config
         self.state = state
 
+    def format_query(self, wheres, startposition, maxresults):
+        bookmark = self.state.get(self.stream_id, {}).get('bookmark') or self.config.get('start_date') # had to update start_date in config
+        wheres += ["Metadata.LastUpdatedTime >= " + "'" +  bookmark + "'"]
+
+        orders = ["Metadata.LastUpdatedTime ASC", "STARTPOSITION " + str(startposition), "MAXRESULTS " + str(maxresults)]
+        where_clause = "WHERE " + " AND ".join(wheres)
+        order_clause = "ORDER BY " + " ".join(orders)
+        query = "SELECT * FROM " + self.query_name + " " + where_clause + " " + order_clause
+        return query
+
+
 # Query Building Notes
 #max-results 200
 # startpostiion start at 1
@@ -31,9 +42,33 @@ class Accounts(Stream):
     replication_method = 'INCREMENTAL'
     replication_keys = ['LastUpdatedTime']
 
+    # capitalization appears to not matter
+    query_name = 'Account'
+
+    # def sync(self):
+    #     additional_wheres = ["Active IN (true, false)"]
+    #     additional_orders = []
+    #     query = self.format_query(additional_wheres, additional_orders)
+    #     for rec in self.client.get(self.endpoint.format(self.client.realm_id), params={"query": "SELECT * FROM Account WHERE Active IN (true, false)"}).get('QueryResponse', {}).get('Account'):
+    #         yield rec
+
     def sync(self):
-        for rec in self.client.get(self.endpoint.format(self.client.realm_id), params={"query": "SELECT * FROM Account WHERE Active IN (true, false)"}).get('QueryResponse', {}).get('Account'):
+        additional_wheres = ["Active IN (true, false)"]
+        startposition = 1
+        maxresults = 90 # this could be global maybe
+        query = self.format_query(additional_wheres, startposition, maxresults)
+
+        # TODO: We can do better here
+        results = self.client.get(self.endpoint.format(self.client.realm_id), params={"query": query}).get('QueryResponse',{}).get('Account', [])
+        for rec in results:
             yield rec
+
+        while len(results) == maxresults:
+            startposition += len(results)
+            query = self.format_query(additional_wheres,  startposition, maxresults)
+            results = self.client.get(self.endpoint.format(self.client.realm_id), params={"query": query}).get('QueryResponse',{}).get('Account', [])
+            for rec in results:
+                yield rec
 
 
 class Invoices(Stream):
