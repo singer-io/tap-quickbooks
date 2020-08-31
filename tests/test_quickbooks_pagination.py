f0feb0b0379340ce9de87e6564eb35b03c5184af
Kyle Speer <kspeer@stitchdata.com>
2020-08-31 12:22:42 -0400

    alter existing assertions to be driven by expectation methods, explicitly verify we exceed pagination threshold

--- tests/test_quickbooks_pagination.py
+++ tests/test_quickbooks_pagination.py
@@ -8,17 +8,10 @@ class TestQuickbooksPagination(TestQuickbooksBase):
     def name(self):
         return "tap_tester_quickbooks_combined_test"
 
-    def expected_replication_keys(self):
-        """
-        return a dictionary with key of table name
-        and value as a set of replication key fields
-        """
-        return {table: properties.get(self.REPLICATION_KEYS, set())
-                for table, properties
-                in self.expected_metadata().items()}
 
     def expected_streams(self):
         return {'accounts'}
+
 
     def get_properties(self):
         return {
@@ -26,6 +19,7 @@ class TestQuickbooksPagination(TestQuickbooksBase):
             'sandbox': 'true',
             'max_results': '10'
         }
+
 
     def test_run(self):
         conn_id = self.ensure_connection()
@@ -40,9 +34,10 @@ class TestQuickbooksPagination(TestQuickbooksBase):
         found_catalogs = menagerie.get_catalogs(conn_id)
         self.assertGreater(len(found_catalogs), 0, msg="unable to locate schemas for connection {}".format(conn_id))
 
-        # Select only the Accounts table
-        accounts_entry = [ce for ce in found_catalogs if ce['tap_stream_id'] == "accounts"]
-        self.select_all_streams_and_fields(conn_id, accounts_entry)
+        # Select only the expected streams tables
+        expected_streams = self.expected_streams()
+        catalog_entries = [ce for ce in found_catalogs if ce['tap_stream_id'] in expected_streams]
+        self.select_all_streams_and_fields(conn_id, catalog_entries)
 
         # Run a sync job using orchestrator
         sync_job_name = runner.run_sync_mode(self, conn_id)
@@ -50,10 +45,21 @@ class TestQuickbooksPagination(TestQuickbooksBase):
         # Verify tap and target exit codes
         exit_status = menagerie.get_exit_status(conn_id, sync_job_name)
         menagerie.verify_sync_exit_status(self, exit_status, sync_job_name)
-
-        # Verify actual rows were synced
         sync_record_count = runner.examine_target_output_file(
             self, conn_id, self.expected_streams(), self.expected_primary_keys())
 
-        # Examine target output
-        self.assertEqual(sync_record_count, {'accounts': 92})
+        # Test by stream
+        for stream in self.expected_streams():
+            with self.subTest(stream=stream):
+
+                record_count = sync_record_count.get(stream, 0)
+
+                expected_count = self.minimum_record_count_by_stream().get(stream)
+
+                # Verify the sync meets or exceeds the default record count
+                self.assertLessEqual(expected_count, record_count)
+
+                # Verify the number or records exceeds the max_results (api limit)
+                api_limit = self.get_properties().get('max_results')
+                self.assertGreater(record_count, api_limit,
+                                   msg="Record count not large enough to gaurantee pagination.")
