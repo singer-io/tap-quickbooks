816c33fb2d27574cfe7fb53c4b5838c3c577a70c
Zach Harris <zharris@talend.com>
2021-03-23 14:57:39 +0000

    bump singer to 5.12.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name='tap-quickbooks',
-      version='1.0.3',
+      version='1.0.4',
       description='Singer.io tap for extracting data from the Quickbooks API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_quickbooks'],
       install_requires=[
-          'singer-python==5.9.0',
+          'singer-python==5.12.1',
           'requests==2.23.0',
           'requests_oauthlib==1.3.0',
       ],
