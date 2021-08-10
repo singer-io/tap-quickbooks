4367e60ad54166bb1868271ed87f33e22c87be26
dbshah1212 <root@ubuntu16.cdsys.local>
2021-08-10 16:09:54 +0530

    TDL-13968: Removed dublicacy of all Ref schemas.

--- tap_quickbooks/discover.py
+++ tap_quickbooks/discover.py
@@ -47,10 +47,14 @@ def do_discover():
         ))
         # Set the replication_key MetaData to automatic as well
         mdata = metadata.write(mdata, ('properties', stream.replication_keys[0]), 'inclusion', 'automatic')
+        ref_schema = singer.utils.load_json(
+            os.path.normpath(
+                os.path.join(_get_abs_path("schemas/ref_schema.json"))))
+        refs = {"ref_schema.json": ref_schema}
         catalog_entry = {
             "stream": stream_name,
             "tap_stream_id": stream_name,
-            "schema": schema,
+            "schema": singer.resolve_schema_references(schema, refs),
             "metadata": metadata.to_list(mdata),
             "key_properties": stream.key_properties
         }
