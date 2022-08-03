712d943c31d455067f9d8952244f7029afca9cf3
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-08-03 12:28:09 +0530

    updated readme file, init and added unittest

---
+++ tests/unittests/test_init.py
@@ -0,0 +1,53 @@
+from singer.catalog import Catalog
+import unittest
+from unittest import mock
+from tap_quickbooks import main, do_discover
+from parameterized import parameterized
+
+class Parse_Args:
+    """Mocked parse args"""
+    
+    def __init__(self,discover=False,state = None, catalog = None,properties= None,config_path = None) -> None:
+        self.discover = discover
+        self.state = state
+        self.config = {}
+        self.catalog = catalog
+        self.properties = properties
+        self.config_path = config_path
+
+class TestQuickbooksInit(unittest.TestCase):
+
+    @parameterized.expand([ # test_name, [discover_flag, catalog_flag], [do_discover, do_sync]
+        ['do_discover_called',[True,True],[True,False]],
+        ['do_sync_called',[False,True],[False,True]],
+        ['do_discover_and_do_sync_called',[False,False],[True,True]]
+    ])
+    @mock.patch('singer.parse_args')
+    @mock.patch('tap_quickbooks.write_catalog')
+    @mock.patch('tap_quickbooks.do_discover')
+    @mock.patch('tap_quickbooks.do_sync')
+    @mock.patch('tap_quickbooks.QuickbooksClient')
+    def test_init(self,test_name,test_data,exp,mocked_client,mocked_sync,mocked_discover,mock_catalog,mock_args):
+        """Test init with various flag scenarios"""
+
+        mock_args.return_value = Parse_Args(discover=test_data[0],catalog=test_data[1])
+        main()
+        self.assertEqual(mocked_discover.called,exp[0])
+        self.assertEqual(mocked_sync.called,exp[1])
+
+    def test_discover_coverage(self):
+        """Test catalog obtained from discover is the instance of singer Catalog"""
+
+        catalog  = do_discover()
+        self.assertIsInstance(catalog, Catalog)
+
+    @mock.patch('singer.parse_args')
+    @mock.patch('tap_quickbooks.QuickbooksClient')
+    def test_init_exception(self,mock_client,mock_args):
+        """Test init raises an exception on passing properties flag """
+
+        mock_args.return_value = Parse_Args(catalog=False,properties=True)
+        with self.assertRaises(Exception) as e:
+            main()
+        expected_message = "DEPRECATED: Use of the 'properties' parameter is not supported. Please use --catalog instead"
+        self.assertEqual(str(e.exception),expected_message)
