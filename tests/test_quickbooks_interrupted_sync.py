a3398e8c83dc2c69f9dddbb311c1c319308af3ba
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-08-10 14:51:27 +0530

    updated interrupted sync test

--- tests/test_quickbooks_interrupted_sync.py
+++ tests/test_quickbooks_interrupted_sync.py
@@ -41,7 +41,7 @@ class TestQuickbooksInterruptedSyncTest(TestQuickbooksBase):
         self.start_date = "2021-01-01T00:00:00Z"
         start_date_datetime = dt.strptime(self.start_date, "%Y-%m-%dT%H:%M:%SZ")
 
-        conn_id = self.ensure_connection()
+        conn_id = self.ensure_connection(original=False)
 
         expected_streams = {"accounts", "bill_payments", "payments"}
 
