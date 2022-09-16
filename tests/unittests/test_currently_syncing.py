5490d92491f1c73916500358f2ba8c97c15e9694
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-09-16 12:43:17 +0530

    formatted test case

--- tests/unittests/test_currently_syncing.py
+++ tests/unittests/test_currently_syncing.py
@@ -4,6 +4,7 @@ from tap_quickbooks.client import QuickbooksClient
 from tap_quickbooks.sync import do_sync
 
 class Schema:
+    """Mock class for the schema"""
     def __init__(self, stream):
         self.stream = stream
 
@@ -11,6 +12,7 @@ class Schema:
         return {'stream': self.stream}
 
 class Stream:
+    """Mock class for the stream"""
     def __init__(self, stream_name):
         self.tap_stream_id = stream_name
         self.stream = stream_name
@@ -19,6 +21,7 @@ class Stream:
         self.metadata = {}
 
 class Catalog:
+    """Mock class for the singer catalog"""
     def __init__(self, streams):
         self.streams = streams
 
@@ -30,13 +33,12 @@ class Catalog:
         return Stream(stream_name)
 
 class TestCurrentlySuncing(unittest.TestCase):
-    """
-        Test cases to check the currently syncing is working as expected
-    """
+
     @mock.patch("tap_quickbooks.streams.Stream.sync")
-    @mock.patch("singer.set_currently_syncing", return_value = {})
-    @mock.patch("tap_quickbooks.client.QuickbooksClient.__init__", return_value = None)
+    @mock.patch("singer.set_currently_syncing", return_value={})
+    @mock.patch("tap_quickbooks.client.QuickbooksClient.__init__", return_value=None)
     def test_currently_syncing(self, mocked_stream_sync, mocked_currently_syncing, mocked_quickbooks_client):
+        """Test case to check the currently syncing is working as expected"""
         config = {
             "client_id": "test_client_id",
             "start_date": "2012-08-21T00:00:00Z",
@@ -47,8 +49,11 @@ class TestCurrentlySuncing(unittest.TestCase):
             "realm_id": "12345"
         }
         state = {}
+
         client = QuickbooksClient("/opt/code/config.json", config)
-        do_sync(client, config, state, Catalog(['bills', 'budgets', 'invoices']))
+
+        do_sync(client=client, config=config, state=state, catalog=Catalog(
+            ['bills', 'budgets', 'invoices']))
 
         # Verify the call count
         self.assertEqual(mocked_currently_syncing.call_count, 4)
