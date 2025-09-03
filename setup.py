a535dd259f9bbe050232a2235a2d5b5033409ff7
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2025-09-03 19:50:25 +0000

    Changelog and version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-quickbooks',
-      version='2.3.1',
+      version='2.3.2',
       description='Singer.io tap for extracting data from the Quickbooks API',
       author='Stitch',
       url='http://singer.io',
