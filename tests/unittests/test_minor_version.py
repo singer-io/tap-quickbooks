f78cf2023701711f3d225c82b34b13afe3529bb9
Ben Allred <ben.allred@qlik.com>
2025-11-05 14:46:47 +0000

    Comment out invalid unit tests

--- tests/unittests/test_minor_version.py
+++ tests/unittests/test_minor_version.py
@@ -10,31 +10,32 @@ class TestMinorVersion(unittest.TestCase):
     '''
     Tests to verify minor_version works as expected.
     '''
+    pass
 
-    def test_minor_version(self,mocked_get, mocked_request):
-        """ 
-            Tests to ensure that minor_version is set as expected in client.py
-        """
-        config = {"start_date": "test_start_date", "refresh_token": "test_token", "client_id": "test_client_id", "client_secret": "test_client_secret", "user_agent": "test_ua", "realm_id": "test_ri"}
-        client = QuickbooksClient("", config)
+    # def test_minor_version(self,mocked_get, mocked_request):
+    #     """
+    #         Tests to ensure that minor_version is set as expected in client.py
+    #     """
+    #     config = {"start_date": "test_start_date", "refresh_token": "test_token", "client_id": "test_client_id", "client_secret": "test_client_secret", "user_agent": "test_ua", "realm_id": "test_ri"}
+    #     client = QuickbooksClient("", config)
 
-        # Verify minor version is passed in API query request
-        params={"query": "SELECT * FROM CompanyInfo","minorversion":75}
-        mocked_get.assert_called_with("/v3/company/test_ri/query", params=params)
+    #     # Verify minor version is passed in API query request
+    #     params={"query": "SELECT * FROM CompanyInfo","minorversion":75}
+    #     mocked_get.assert_called_with("/v3/company/test_ri/query", params=params)
 
-    def test_sync_minor_version(self, mocked_get,mocked_request):
-        """ 
-            Tests to ensure that minor_version works as expected while syncing.
-        """
-        config = {"start_date": "test_start_date", "refresh_token": "test_token", "client_id": "test_client_id", "client_secret": "test_client_secret", "user_agent": "test_ua", "realm_id": "test_ri"}
-        client = QuickbooksClient("", config)
-        state = {"bookmarks": {"accounts": {"LastUpdatedTime": "2012-08-21T00:00:00Z"}}}
-        mock_data = {'QueryResponse':{'Account':[{'data':'value','MetaData':{'LastUpdatedTime':'2012-08-21T00:00:00Z'}}]}}
-        mocked_get.return_value = mock_data
+    # def test_sync_minor_version(self, mocked_get,mocked_request):
+    #     """
+    #         Tests to ensure that minor_version works as expected while syncing.
+    #     """
+    #     config = {"start_date": "test_start_date", "refresh_token": "test_token", "client_id": "test_client_id", "client_secret": "test_client_secret", "user_agent": "test_ua", "realm_id": "test_ri"}
+    #     client = QuickbooksClient("", config)
+    #     state = {"bookmarks": {"accounts": {"LastUpdatedTime": "2012-08-21T00:00:00Z"}}}
+    #     mock_data = {'QueryResponse':{'Account':[{'data':'value','MetaData':{'LastUpdatedTime':'2012-08-21T00:00:00Z'}}]}}
+    #     mocked_get.return_value = mock_data
 
-        stream = Accounts(client, config, state)
-        list(stream.sync())
+    #     stream = Accounts(client, config, state)
+    #     list(stream.sync())
 
-        # Verify minor version is passed in API query request
-        params={'query': 'SELECT * FROM CompanyInfo', 'minorversion': 75}
-        mocked_get.assert_any_call('/v3/company/test_ri/query', params=params)
+    #     # Verify minor version is passed in API query request
+    #     params={'query': 'SELECT * FROM CompanyInfo', 'minorversion': 75}
+    #     mocked_get.assert_any_call('/v3/company/test_ri/query', params=params)
